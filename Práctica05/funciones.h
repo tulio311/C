f1af237c855d999af6989447e693fa80fe99e816
tulio311 <124656639+tulio311@users.noreply.github.com>
2023-02-15 00:32:39 -0600

    Add files via upload

---
+++ Práctica05/funciones.h
@@ -0,0 +1,7 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+int cuadratica(double a, double b, double c, double*x1, double *x2);
+void sumarPolinomios(int n, double *c1, int m, double*c2, int *k, double *c3);
+
+#endif
